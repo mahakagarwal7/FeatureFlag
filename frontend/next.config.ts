ac649318305f51d3fb3b6e90ddc6034a6feb77d8
mahakagarwal7 <mahakagrwl07@gmail.com>
2026-04-26 03:09:31 +0530

    Frontend issue fixed

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    // Turbopack settings for Next.js 15+
+  },
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
 };
 
 export default nextConfig;
